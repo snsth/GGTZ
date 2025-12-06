e8f2ac366c9983f877d3ab94ac6337cf2fe0a92e
BaekSeungJin <bsj020419@naver.com>
2025-12-07 03:57:22 +0900

    update

---
+++ Assets/UI/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Camera;
+    public GameObject Golem_Boss;
+    public GameObject Player;
+    public GameObject MenuPanel;
+    public GameObject GamePanel;
+
+    public GameObject Title;
+    public GameObject Option;
+    public GameObject Keysetting;
+    public Image Guard;
+    public Image Heal;
+    public Image Buff;
+    public Image Ultimate;
+    public RectTransform playerHP;
+    public RectTransform playerMP;
+
+
+    public void GameStart()
+    {
+        Camera.SetActive(false);
+
+        Golem_Boss.SetActive(true);
+        Player.SetActive(true);
+
+        MenuPanel.SetActive(false);
+        GamePanel.SetActive(true);
+    }
+
+    public void GameOption()
+    {
+        Title.SetActive(false);
+        Option.SetActive(true);
+    }
+
+    public void GameOptionR()
+    {
+        Title.SetActive(true);
+        Option.SetActive(false);
+    }
+
+    public void GameKeySetting()
+    {
+        Title.SetActive(false);
+        Keysetting.SetActive(true);
+    }
+
+    public void GameKeySettingR()
+    {
+        Title.SetActive(true);
+        Keysetting.SetActive(false);
+    }
+
+}
